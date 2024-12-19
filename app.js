7140387d6072bfca6609b9f4e00f9563cdbc74a9
moyd1nov <aloneboyrkbt6@example.com>
2024-12-19 15:16:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+function hisobla1(){
+    var a = 10;
+    var b = 5; 
+    var c = 0;
+
+   c = a + b - (2 * a + b ) / 100 + a * b + 1024
+
+   console.log(c)
+
+}
+function hisobla2(){
+    var a = 20;
+    var b = 10; 
+    var c = 0;
+
+    c = a / b + b / a - 2 / a
+
+   console.log(c)
+   
+}
+function hisobla3(){
+    var a = 20;
+    var b = 10; 
+    var c = 0;
+
+    c = a + b * 1000 - 1024 + ( a + b )
+
+   console.log(c)
+    
+}
+function hisobla4(){
+    var a = prompt('a sonini kiriting')*1;
+    var b = prompt('b sonini kiriting')*1; 
+    var c = 0;
+
+   c = a + b - (2 * a + b ) / 100 + a * b + 1024
+
+   alert(c)
+
+}
+function hisobla5(){
+    var a = prompt('a sonini kiriting')*1;
+    var b = prompt('b sonini kiriting')*1; 
+    var c = 0;
+
+    c = a / b + b / a - 2 / a
+
+   alert(c)
+  
+}
+function hisobla6(){
+    var a = prompt('a sonini kiriting')*1;
+    var b = prompt('b sonini kiriting')*1; 
+    var c = 0;
+
+    c = a + b * 1000 - 1024 + ( a + b )
+
+   alert(c)
+  
+   
+}
